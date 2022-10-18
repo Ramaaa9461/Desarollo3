3a29a43a27f8644b8a5b8653d8c3d009c3cb0449
Ramiro Guzman <rami.guzmi@gmail.com>
2022-10-18 10:27:51 -0300

    Optimizacion del Boton

--- Prueba camara con terreno/Assets/Game/Scripts/Button/ButtonBase.cs
+++ Prueba camara con terreno/Assets/Game/Scripts/Button/ButtonBase.cs
@@ -1,13 +1,48 @@
+using System.Collections;
 using UnityEngine;
 
-public abstract class ButtonBase : MonoBehaviour
+public class ButtonBase : MonoBehaviour
 {
     [SerializeField] protected LigthButton lightButton;
     [SerializeField] protected TowersLogicBase towerBase;
     [SerializeField] protected ColumnLogicBase columBase;
     [SerializeField] protected TreeGrowth treeGrowth;
+    [SerializeField] GameObject Door;
     protected bool doorIsOpen = false;
+    bool openDoor;
+    public void pressButton()
+    {
+        if (!doorIsOpen)
+        {
+            if (columBase == null)
+            {
+                openDoor = towerBase.CheckWinCondition();
+            }
+            else
+            {
+                openDoor = towerBase.CheckWinCondition() && columBase.CheckWincondition();
 
-    public abstract void pressButton();
+            }
+
+            if (openDoor)
+            {
+                treeGrowth.UpTree();
+
+                StartCoroutine(WaitGrowthTree());
+
+                doorIsOpen = true;
+            }
+        }
+
+        lightButton.ChangeLigthColor(openDoor);
+    }
+
+
+    IEnumerator WaitGrowthTree()
+    {
+        yield return new WaitForSeconds(3);
+
+        Door.GetComponent<OpenDoor>().UpTheDoor();
+    }
 
 }
